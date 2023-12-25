95a76cabbfc927e6bc08f58b8b0f0a53aeec0ea4
huuminh2612 <113404734+huuminh2612@users.noreply.github.com>
2023-12-25 22:00:40 +0700

    Add files via upload

---
+++ module1.py
@@ -0,0 +1,15 @@
+#bài 1 in Hello world ra màn hình (terminal)
+b1=input()
+print(b1)
+#bài 2 khai báo biến
+a = 'Hello world2'
+print(a)
+#Bài 3 biểu thức
+b = 'Hello'
+c = 'World3'
+d=b+" "+c
+print(d)
+#Bài 4 hàm số
+def print_var(var):
+    print(var)
+print_var(d)
